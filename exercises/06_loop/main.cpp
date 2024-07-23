5633fecf32fea5805267e313de02883f77e80242
raisuny <rose@wxm.com>
2024-07-23 21:22:14 +0800

    update

--- exercises/06_loop/main.cpp
+++ exercises/06_loop/main.cpp
@@ -1,11 +1,12 @@
 #include "../exercise.h"
 
 // TODO: 改正函数实现，实现正确的缓存优化斐波那契计算
-static unsigned long long fibonacci(int i) {
+ static unsigned long long fibonacci(int i) {
     // TODO: 为缓存设置正确的初始值
-    static unsigned long long cache[96], cached;
+     static unsigned long long cache[96]={0,1};
+     long long cached=2;
     // TODO: 设置正确的循环条件
-    for (; false; ++cached) {
+    for (; cached<=i; ++cached) {
         cache[cached] = cache[cached - 1] + cache[cached - 2];
     }
     return cache[i];
