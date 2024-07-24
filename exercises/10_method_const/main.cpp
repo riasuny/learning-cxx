794ead2fec5e539afa30d4dbf8548e1617cb3e7a
raisuny <rose@wxm.com>
2024-07-24 17:15:11 +0800

    update

--- exercises/10_method_const/main.cpp
+++ exercises/10_method_const/main.cpp
@@ -1,23 +1,19 @@
-#include "../exercise.h"
 #include<iostream>
 #include <cassert> // 引入 assert 头文件
 #define ASSERT(condition, message) assert(condition && message)
 struct Fibonacci {
     int numbers[11];
     // TODO: 修改方法签名和实现，使测试通过
-    constexpr Fibonacci(const int(&arr)[11]){
-    for(int i=0;i<11;++i)
+    constexpr Fibonacci(const int(&arr)[11]) : numbers{ arr[0], arr[1], arr[2], arr[3], arr[4], arr[5], arr[6], arr[7], arr[8], arr[9], arr[10] }
     {
-        numbers[i]=arr[i];
     }
-    }
-    int get(int i) const{
+    int get(int i) const {
         return numbers[i];
     }
 };
 
-int main(int argc, char **argv) {
-    Fibonacci constexpr FIB{{0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55}};
+int main(int argc, char** argv) {
+    Fibonacci constexpr FIB{ {0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55} };
     ASSERT(FIB.get(10) == 55, "fibonacci(10) should be 55");
     std::cout << "fibonacci(10) = " << FIB.get(10) << std::endl;
     return 0;
