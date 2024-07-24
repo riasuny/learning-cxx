c4b73c41b6ca9a8bedda6e3f92986046a9470043
raisuny <rose@wxm.com>
2024-07-24 10:56:15 +0800

    update

--- exercises/08_trivial/main.cpp
+++ exercises/08_trivial/main.cpp
@@ -1,28 +1,26 @@
-#include "../exercise.h"
-#include<iostream>
-// READ: Trivial type <https://learn.microsoft.com/zh-cn/cpp/cpp/trivial-standard-layout-and-pod-types?view=msvc-170>
+#include <iostream>
+#include <cassert> // 包含标准的断言宏 assert
 
 struct FibonacciCache {
-    unsigned long long cache[16]={0,1};
-    int cached=1;
+    unsigned long long cache[16] = {0, 1}; // 初始斐波那契数列的前两个数
+    int cached = 1; // 初始缓存的数的个数
 };
 
-// TODO: 实现正确的缓存优化斐波那契计算
+// 实现缓存优化斐波那契计算
 static unsigned long long fibonacci(FibonacciCache &cache, int i) {
-    if(i<0)
-    return 0;
-    for (; cache.cached<=i&&i>=2; ++cache.cached) {
+    // 如果需要计算的数超过当前缓存的范围，则继续计算
+    for (; i >= cache.cached; ++cache.cached) {
         cache.cache[cache.cached] = cache.cache[cache.cached - 1] + cache.cache[cache.cached - 2];
     }
     return cache.cache[i];
 }
 
 int main(int argc, char **argv) {
-    // TODO: 初始化缓存结构体，使计算正确
-    // NOTICE: C/C++ 中，读取未初始化的变量（包括结构体变量）是未定义行为
-    // READ: 初始化的各种写法 <https://zh.cppreference.com/w/cpp/language/initialization>
     FibonacciCache fib;
-    ASSERT(fibonacci(fib, 10) == 55, "fibonacci(10) should be 55");
+    fib.cached = 2; // 初始已缓存的数为 2，即斐波那契数列的前两个数已经计算
+    
+    assert(fibonacci(fib, 10) == 55 && "fibonacci(10) should be 55");
     std::cout << "fibonacci(10) = " << fibonacci(fib, 10) << std::endl;
+    
     return 0;
 }
--- exercises/09_method/main.cpp
+++ exercises/09_method/main.cpp
@@ -7,9 +7,7 @@ struct Fibonacci {
     // TODO: 实现正确的缓存优化斐波那契计算
     unsigned long long get(int i) {
         Fibonacci c;
-        if(i<0)
-        return 0;
-        for (; c.cached<=i&&i>=2; ++c.cached) {
+        for (; c.cached<=i; ++c.cached) {
             c.cache[c.cached] = c.cache[c.cached - 1] + c.cache[c.cached - 2];
         }
         return c.cache[i];
@@ -19,6 +17,7 @@ struct Fibonacci {
 int main(int argc, char **argv) {
     // TODO: 初始化缓存结构体，使计算正确
     Fibonacci fib;
+    fib.cached=2;
     ASSERT(fib.get(10) == 55, "fibonacci(10) should be 55");
     std::cout << "fibonacci(10) = " << fib.get(10) << std::endl;
     return 0;
