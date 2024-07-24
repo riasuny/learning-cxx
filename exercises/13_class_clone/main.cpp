1f8721b3ea3203b8f8c168719fc9fdc35ae7896e
raisuny <rose@wxm.com>
2024-07-24 21:35:10 +0800

    update

--- exercises/13_class_clone/main.cpp
+++ exercises/13_class_clone/main.cpp
@@ -1,84 +1,67 @@
 #include<iostream>
 #include <cassert> // 引入 assert 头文件
 #define ASSERT(condition, message) assert(condition && message)
-// READ: 移动构造函数 <https://zh.cppreference.com/w/cpp/language/move_constructor>
-// READ: 移动赋值 <https://zh.cppreference.com/w/cpp/language/move_assignment>
-// READ: 运算符重载 <https://zh.cppreference.com/w/cpp/language/operators>
+// READ: 静态字段 <https://zh.cppreference.com/w/cpp/language/static>
+// READ: 虚析构函数 <https://zh.cppreference.com/w/cpp/language/destructor>
 
-class DynFibonacci {
-    size_t* cache;
-    int cached;
+struct A {
+    // TODO: 正确初始化静态字段
+    static int num_a;
 
-public:
-    // TODO: 实现动态设置容量的构造器
-    DynFibonacci(int capacity) noexcept: cache(new size_t[capacity]), cached(0) {
-        if (capacity > 0)
-        {
-            cache[0] = 0;
-            cached = 1;
-        }
-        if (capacity > 1)
-        {
-            cache[1] = 1;
-            cached = 2;
-        }
+    A() {
+        ++num_a;
+    }
+    ~A() {
+        --num_a;
     }
 
-    // TODO: 实现移动构造器
-    DynFibonacci(DynFibonacci && other) noexcept:cache(other.cache), cached(other.cached)
-    {
-        other.cache = nullptr;
-        other.cached = 0;
-    }
-    // TODO: 实现移动赋值
-    DynFibonacci& operator=(DynFibonacci&& other) noexcept {
-        if (this != &other) {
-            delete[] cache;
-            cache = other.cache;
-            cached = other.cached;
-            other.cache = nullptr;
-            other.cached = 0;
-        }
-        return *this;
-    }
-
-    // TODO: 实现析构器，释放缓存空间
-    ~DynFibonacci()
-    {
-        delete[] cache;
-    }
-
-    // TODO: 实现正确的缓存优化斐波那契计算
-    size_t operator[](int i) {
-        for (; cached <= i; ++cached) {
-            if (cached < 2) {
-                cache[cached] = cached; // 前两个斐波那契数直接初始化为索引值
-            } else {
-                cache[cached] = cache[cached - 1] + cache[cached - 2];
-            }
-        }
-        return cache[i];
-    }
-    // NOTICE: 不要修改这个方法
-    bool is_alive() const {
-        return cache;
+    virtual char name() const {
+        return 'A';
     }
 };
+int A::num_a = 0;
+struct B final : public A {
+    // TODO: 正确初始化静态字段
+    static int num_b;
 
+    B() {
+        ++num_b;
+    }
+    ~B() {
+        --num_b;
+    }
+
+    char name() const final {
+        return 'B';
+    }
+};
+int B::num_b = 0;
 int main(int argc, char** argv) {
-    DynFibonacci fib(12);
-    ASSERT(fib[10] == 55, "fibonacci(10) should be 55");
+    auto a = new A;
+    auto b = new B;
+    ASSERT(A::num_a == 1, "Fill in the correct value for A::num_a");
+    ASSERT(B::num_b == 1, "Fill in the correct value for B::num_b");
+    ASSERT(a->name() == 'A', "Fill in the correct value for a->name()");
+    ASSERT(b->name() == 'B', "Fill in the correct value for b->name()");
 
-    DynFibonacci const fib_ = std::move(fib);
-    ASSERT(!fib.is_alive(), "Object moved");
-    ASSERT(fib[10] == 55, "fibonacci(10) should be 55");
+    delete a;
+    delete b;
+    ASSERT(A::num_a == 0, "Every A was destroyed");
+    ASSERT(B::num_b == 0, "Every B was destroyed");
 
-    DynFibonacci fib0(6);
-    DynFibonacci fib1(12);
+    A* ab = new B;// 派生类指针可以随意转换为基类指针
+    ASSERT(A::num_a == 1, "Fill in the correct value for A::num_a");
+    ASSERT(B::num_b == 1, "Fill in the correct value for B::num_b");
+    ASSERT(ab->name() == 'B', "Fill in the correct value for ab->name()");
 
-    fib0 = std::move(fib1);
-    fib0 = std::move(fib0);
-    ASSERT(fib0[10] == 55, "fibonacci(10) should be 55");
+    // TODO: 基类指针无法随意转换为派生类指针，补全正确的转换语句
+    B& bb = dynamic_cast<B&>(*ab);
+    ASSERT(bb.name() == 'B', "Fill in the correct value for bb->name()");
+
+    // TODO: ---- 以下代码不要修改，通过改正类定义解决编译问题 ----
+    delete ab;// 通过指针可以删除指向的对象，即使是多态对象
+    ASSERT(A::num_a == 0, "Every A was destroyed");
+    ASSERT(B::num_b == 0, "Every B was destroyed");
 
     return 0;
 }
