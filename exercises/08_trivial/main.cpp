8c6f603f4c14c26be5ea2882ecea0215caf1fe46
raisuny <rose@wxm.com>
2024-07-24 11:01:19 +0800

    update

--- exercises/08_trivial/main.cpp
+++ exercises/08_trivial/main.cpp
@@ -1,11 +1,9 @@
-#include <iostream>
-#include <cassert> // 包含标准的断言宏 assert
-
+#include "../exercise.h"
+#include<cassert>
 struct FibonacciCache {
     unsigned long long cache[16] = {0, 1}; // 初始斐波那契数列的前两个数
     int cached = 1; // 初始缓存的数的个数
 };
-
 // 实现缓存优化斐波那契计算
 static unsigned long long fibonacci(FibonacciCache &cache, int i) {
     // 如果需要计算的数超过当前缓存的范围，则继续计算
@@ -14,13 +12,10 @@ static unsigned long long fibonacci(FibonacciCache &cache, int i) {
     }
     return cache.cache[i];
 }
-
 int main(int argc, char **argv) {
     FibonacciCache fib;
     fib.cached = 2; // 初始已缓存的数为 2，即斐波那契数列的前两个数已经计算
-    
     assert(fibonacci(fib, 10) == 55 && "fibonacci(10) should be 55");
     std::cout << "fibonacci(10) = " << fibonacci(fib, 10) << std::endl;
-    
     return 0;
 }
