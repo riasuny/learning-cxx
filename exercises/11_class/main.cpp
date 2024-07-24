f8972f5894f27a1b4a4089dcc677b9fe7df17fc7
raisuny <rose@wxm.com>
2024-07-24 16:04:26 +0800

    update

--- exercises/11_class/main.cpp
+++ exercises/11_class/main.cpp
@@ -1,26 +1,19 @@
-#include<iostream>
-// C++ 中，`class` 和 `struct` 之间的**唯一区别**是
-// `class` 默认访问控制符是 `private`，
-// `struct` 默认访问控制符是 `public`。
-// READ: 访问说明符 <https://zh.cppreference.com/w/cpp/language/access>
+#include <iostream>
+#include <cassert> // 引入 assert 头文件
 
-// 这个 class 中的字段被 private 修饰，只能在 class 内部访问。
-// 因此必须提供构造器来初始化字段。
-// READ: 构造器 <https://zh.cppreference.com/w/cpp/language/constructor>
+#define ASSERT(condition, message) assert(condition && message)
+
 class Fibonacci {
     size_t cache[16];
     int cached;
 
 public:
-    // TODO: 实现构造器
-    // Fibonacci()
-    Fibonacci()
-    {
+    Fibonacci() {
         cache[0] = 0;
         cache[1] = 1;
         cached = 2;
     }
-    // TODO: 实现正确的缓存优化斐波那契计算
+
     size_t get(int i) {
         for (; cached <= i; ++cached) {
             cache[cached] = cache[cached - 1] + cache[cached - 2];
@@ -29,9 +22,7 @@ public:
     }
 };
 
-int main(int argc, char** argv) {
-    // 现在类型拥有无参构造器，声明时会直接调用。
-    // 这个写法不再是未定义行为了。
+int main() {
     Fibonacci fib;
     ASSERT(fib.get(10) == 55, "fibonacci(10) should be 55");
     std::cout << "fibonacci(10) = " << fib.get(10) << std::endl;
--- exercises/12_class_destruct/main.cpp
+++ exercises/12_class_destruct/main.cpp
@@ -1,33 +1,42 @@
-#include "../exercise.h"
-
+#include<iostream>
 // READ: 析构函数 <https://zh.cppreference.com/w/cpp/language/destructor>
 
 /// @brief 任意缓存容量的斐波那契类型。
 /// @details 可以在构造时传入缓存容量，因此需要动态分配缓存空间。
 class DynFibonacci {
-    size_t *cache;
+    size_t* cache;
     int cached;
 
 public:
     // TODO: 实现动态设置容量的构造器
-    DynFibonacci(int capacity): cache(new size_t[capacity]), cached(2) {}
+    DynFibonacci(int capacity) : cache(new size_t[capacity]), cached(0) {
+        if (capacity > 0)
+        {
+            cache[0] = 0;
+        }
+        if (capacity > 1)
+        {
+            cache[1] = 1;
+            cached = 2;
+        }
+    }
 
     // TODO: 实现析构器，释放缓存空间
     ~DynFibonacci()
     {
-        delete []cache;
+        delete[] cache;
     }
 
     // TODO: 实现正确的缓存优化斐波那契计算
     size_t get(int i) {
-        for (; cached<=i; ++cached) {
+        for (; cached <= i; ++cached) {
             cache[cached] = cache[cached - 1] + cache[cached - 2];
         }
         return cache[i];
     }
 };
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv) {
     DynFibonacci fib(12);
     ASSERT(fib.get(10) == 55, "fibonacci(10) should be 55");
     std::cout << "fibonacci(10) = " << fib.get(10) << std::endl;
