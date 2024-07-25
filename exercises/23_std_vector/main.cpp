c813b742d167b6630436f9df154f96af1a6960ee
raisuny <rose@wxm.com>
2024-07-25 15:55:08 +0800

    update

--- exercises/23_std_vector/main.cpp
+++ exercises/23_std_vector/main.cpp
@@ -8,24 +8,24 @@
 int main(int argc, char **argv) {
     {
         std::vector<int> vec{1, 2, 3, 4, 5};
-        ASSERT(vec.size() == ?, "Fill in the correct value.");
+        ASSERT(vec.size() == 5, "Fill in the correct value.");
         // THINK: `std::vector` 的大小是什么意思？与什么有关？
-        ASSERT(sizeof(vec) == ?, "Fill in the correct value.");
+        ASSERT(sizeof(vec) == 5*sizoef(int), "Fill in the correct value.");
         int ans[]{1, 2, 3, 4, 5};
-        ASSERT(std::memcmp(vec.?, ans, sizeof(ans)) == 0, "Fill in the correct values.");
+        ASSERT(std::memcmp(vec.data(), ans, sizeof(ans)) == 0, "Fill in the correct values.");
     }
     {
         std::vector<double> vec{1, 2, 3, 4, 5};
         {
-            ASSERT(vec.size() == ?, "Fill in the correct value.");
-            ASSERT(sizeof(vec) == ?, "Fill in the correct value.");
+            ASSERT(vec.size() == 5, "Fill in the correct value.");
+            ASSERT(sizeof(vec) == 5*sizoef(double), "Fill in the correct value.");
             double ans[]{1, 2, 3, 4, 5};
-            ASSERT(std::memcmp(vec.?, ans, sizeof(ans)) == 0, "Fill in the correct values.");
+            ASSERT(std::memcmp(vec.data(), ans, sizeof(ans)) == 0, "Fill in the correct values.");
         }
         {
             vec.push_back(6);
-            ASSERT(vec.size() == ?, "Fill in the correct value.");
-            ASSERT(sizeof(vec) == ?, "Fill in the correct value.");
+            ASSERT(vec.size() == 11, "Fill in the correct value.");
+            ASSERT(sizeof(vec) ==11*sizeof(double), "Fill in the correct value.");
             vec.pop_back();
             ASSERT(vec.size() == ?, "Fill in the correct value.");
             ASSERT(sizeof(vec) == ?, "Fill in the correct value.");
