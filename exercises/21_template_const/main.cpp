26281df8bebd1b08b9884e01d75c7f197c33b22e
raisuny <rose@wxm.com>
2024-07-25 16:49:19 +0800

    update

--- exercises/21_template_const/main.cpp
+++ exercises/21_template_const/main.cpp
@@ -1,17 +1,18 @@
-﻿#include "../exercise.h"
+﻿#include<iostream>
 #include <cstring>
-
+#include <cassert> // 引入 assert 头文件
+#define ASSERT(condition, message) assert(condition && message)
 // READ: 模板非类型实参 <https://zh.cppreference.com/w/cpp/language/template_parameters#%E6%A8%A1%E6%9D%BF%E9%9D%9E%E7%B1%BB%E5%9E%8B%E5%AE%9E%E5%8F%82>
 
 template<unsigned int N, class T>
 struct Tensor {
     unsigned int shape[N];
-    T *data;
+    T* data;
 
     Tensor(unsigned int const shape_[N]) {
         unsigned int size = 1;
         // TODO: 填入正确的 shape 并计算 size
-        for (int i = 0;i < N;i++)
+        for (unsigned int i = 0;i < N;++i)
         {
             shape[i] = shape_[i];
             size *= shape[i];
@@ -24,13 +25,13 @@ struct Tensor {
     }
 
     // 为了保持简单，禁止复制和移动
-    Tensor(Tensor const &) = delete;
-    Tensor(Tensor &&) noexcept = delete;
+    Tensor(Tensor const&) = delete;
+    Tensor(Tensor&&) noexcept = delete;
 
-    T &operator[](unsigned int const indices[N]) {
+    T& operator[](unsigned int const indices[N]) {
         return data[data_index(indices)];
     }
-    T const &operator[](unsigned int const indices[N]) const {
+    T const& operator[](unsigned int const indices[N]) const {
         return data[data_index(indices)];
     }
 
@@ -39,37 +40,37 @@ private:
         unsigned int index = 0;
         unsigned int stride = 1;
         for (unsigned int i = 0; i < N; ++i) {
-            ASSERT(indices[i] < shape[i]);
+            ASSERT(indices[i] < shape[i],"Index out of range");
             index += indices[i] * stride;
             stride *= shape[i];
-        } 
+        }
         return index;
     }
 };
 
 // ---- 不要修改以下代码 ----
-int main(int argc, char **argv) {
+int main(int argc, char** argv) {
     {
-        unsigned int shape[]{2, 3, 4, 5};
+        unsigned int shape[]{ 2, 3, 4, 5 };
         auto tensor = Tensor<4, int>(shape);
 
-        unsigned int i0[]{0, 0, 0, 0};
+        unsigned int i0[]{ 0, 0, 0, 0 };
         tensor[i0] = 1;
         ASSERT(tensor[i0] == 1, "tensor[i0] should be 1");
 
-        unsigned int i1[]{1, 2, 3, 4};
+        unsigned int i1[]{ 1, 2, 3, 4 };
         tensor[i0] = 2;
         ASSERT(tensor[i0] == 2, "tensor[i1] should be 2");
     }
     {
-        unsigned int shape[]{7, 8, 128};
+        unsigned int shape[]{ 7, 8, 128 };
         auto tensor = Tensor<3, float>(shape);
 
-        unsigned int i0[]{0, 0, 0};
+        unsigned int i0[]{ 0, 0, 0 };
         tensor[i0] = 1.f;
         ASSERT(tensor[i0] == 1.f, "tensor[i0] should be 1");
 
-        unsigned int i1[]{3, 4, 99};
+        unsigned int i1[]{ 3, 4, 99 };
         tensor[i0] = 2.f;
         ASSERT(tensor[i0] == 2.f, "tensor[i1] should be 2");
     }
--- exercises/22_std_array/main.cpp
+++ exercises/22_std_array/main.cpp
@@ -9,9 +9,9 @@ int main(int argc, char **argv) {
     {
         std::array<int, 5> arr{{1, 2, 3, 4, 5}};
         ASSERT(arr.size() == 5, "Fill in the correct value.");
-        ASSERT(sizeof(arr) == 20, "Fill in the correct value.");
+        ASSERT(sizeof(arr) == 5*sizeof(int), "Fill in the correct value.");
         int ans[]{1, 2, 3, 4, 5};
-        ASSERT(std::memcmp(arr.data(), ans, 5*sizoef()) == 0, "Fill in the correct values.");
+        ASSERT(std::memcmp(arr.data(), ans, 5*sizoef(int)) == 0, "Fill in the correct values.");
     }
     {
         std::array<double, 8> arr;
